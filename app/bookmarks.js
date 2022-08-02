19581004b968e3c797e7713eac41f3b374c2308b
mrbrist <bristol2801@gmail.com>
2022-08-02 16:12:59 +0100

    Initial UI Sidebar

---
+++ app/bookmarks.js
@@ -0,0 +1,4 @@
+let container = document.createElement('div');
+container.className = "bookmarks-container"
+container.textContent = "This page has been eaten";
+document.body.appendChild(container);
